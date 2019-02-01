b66f5b8d273cfc7f822c9db4b6dbddb852169ecd
Athmika <athmikas@gmail.com>
2019-01-31 19:46:08 -0500

    optimization + cards.c split into functions

--- exercises/ex01/cards.c
+++ exercises/ex01/cards.c
@@ -1,13 +1,57 @@
+/* 
+This program helps users improve their chances at blackjack by card counting. 
+Code from Head First C page 37, modified by Athmika
+*/
+
+
 #include <stdio.h>
 #include <stdlib.h>
+
+/*This function displays the current count to the command prompt*/
+void displayCount(int count)
+{
+	printf("Current count: %i\n", count);
+}
+
+/*This function either increments or decrements the count depending on the value of the card*/
+int updateCount(int val, int count)
+{
+	if ((val > 2) && (val < 7)) 
+	{
+		count++;
+	} 
+	else if (val == 10) 
+	{
+		count--;
+	}
+
+	return count;
+}
+
+/*This function makes sure that the user doesn't enter an invalid card*/
+int throwException(char card_name[])
+{
+	int val = atoi(card_name);
+	if ((val < 1) || (val > 10)) 
+	{
+		puts("I don't understand that value!");
+		return 1;
+	}
+	return 0;
+}
+
 int main()
 {
-	char card_name[3];
 	int count = 0;
+	char card_name[3];
+
 	do {
-	puts("Enter the card_name: ");
+
+    puts("Enter the card_name: ");
 	scanf("%2s", card_name);
+
 	int val = 0;
+
 	switch(card_name[0]) {
 		case 'K':
 		case 'Q':
@@ -20,25 +64,14 @@ int main()
 		case 'X':
 			continue;
 		default:
-			val = atoi(card_name);
-			if ((val < 1) || (val > 10)) 
-			{
-				puts("I don't understand that value!");
-				continue;
-			}
+		    val = atoi(card_name);
+			if (throwException(card_name) == 1) {continue;}
+		}
 
-			}
+	count = updateCount(val,count);
+	displayCount(count);
 
-	if ((val > 2) && (val < 7)) 
-	{
-		count++;
-	} 
-	else if (val == 10) 
-	{
-		count--;
-	}
-	printf("Current count: %i\n", count);
-	} while ( card_name[0] != 'X');
+	} while (card_name[0] != 'X');
 	
 	return 0;
 }
